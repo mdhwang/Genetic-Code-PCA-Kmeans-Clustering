8f50b2ea5fa8ce7a6310e65af36ff3f539d9a7ee
mdhwang <mattdhwang@gmail.com>
2020-05-05 12:21:46 -0700

    updated PCA with standardization and normalization options - updated graphing output

--- src/PCAFreq.py
+++ src/PCAFreq.py
@@ -1,8 +1,28 @@
 import numpy as np
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
-def PCAFreq(freqTable):
-    X = np.array([list(x.values()) for x in freqTable])
+def PCAFreq(freqTable,preprocessing):
+    '''
+    Takes in frequency table dict, converts to nparray, normalizes data, runs PCA for 
+    2 principal components and returns data for plotting.
+    '''
+    
+    # Convert dict to nparray
+    arr = np.array([list(x.values()) for x in freqTable])
+    arr = arr.astype(float)
+
+    if preprocessing == "Normalize":
+        # Normalize values
+        X = arr / np.linalg.norm(arr)
+    elif preprocessing == "Standardize":
+        # Standardize values
+        scaler = StandardScaler()
+        scaler.fit(arr)
+        X = scaler.transform(arr)
+    else:
+        X = arr
+
     pca = PCA(n_components=2)
     model = pca.fit_transform(X)
     return model
